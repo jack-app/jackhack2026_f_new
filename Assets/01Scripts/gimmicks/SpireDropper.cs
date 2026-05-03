58239cc14c4a9769e79cff500b1638e9872e9771
TaniKevin <haniaonori312@gmail.com>
2026-05-03 11:16:25 +0900

    ギミック制作

--- Assets/01Scripts/gimmicks/SpireDropper.cs
+++ Assets/01Scripts/gimmicks/SpireDropper.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class SpireDropper : MonoBehaviour
 {
+    [SerializeField] float shootForce;
+    public GameObject spirePrefab;
+    DropperSwitch drSwitch;
+    bool shooting;
+    GameObject createSpirePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       drSwitch = GameObject.Find("DropperSwitch").GetComponent<DropperSwitch>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(drSwitch.spireDrop == true)
+        {
+            createSpirePrefab = Instantiate(spirePrefab);
+            createSpirePrefab.transform.position = this.transform.position;
+            drSwitch.spireDrop = false;
+            shooting = true;
+        }
+        if (shooting == true && createSpirePrefab != null)
+        {
+            createSpirePrefab.transform.Translate(Vector3.up * shootForce * Time.deltaTime);
+        }
     }
 }
