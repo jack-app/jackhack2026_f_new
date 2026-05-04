4b62cd73547ccafde6234058c5b3f1c03b54d7b1
umee1206 <umee8130@gmail.com>
2026-05-04 11:42:39 +0900

    ステージの完成　BGMの追加

---
+++ Assets/01Scripts/Audio/BGMStart.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BGMStart : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+     [SerializeField]private AudioClip BGM1;
+    void Start()
+    {
+        AudioManager.Instance.PlayBGM(BGM1, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StopMusic()
+    {
+        AudioManager.Instance.StopBGM();
+    }
+}
